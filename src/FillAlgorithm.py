0f314ce3da9905a8977389921b48a39709de90a2
Phil M. Harner <philmh@vt.edu>
2019-12-16 01:29:11 -0500

    Code completion, integration with original problem

--- src/FillAlgorithm.py
+++ src/FillAlgorithm.py
@@ -3,10 +3,11 @@ Created on Dec 10, 2019
 
 @author: Phil M. H.
 '''
+import random
 
 ''''OLD STUFF:count up to 1024 (10 positions, 2 options each
 x = 0,1,2..1024
-#x%(2*index)
+#x%(2*i)
 
  
 for bucketCount in range(0, 10):
@@ -19,34 +20,42 @@ for bucketCount in range(0, 10):
 list = [random.randint(1, maxSize) for _ in range(bucketCount)]
 print(list)'''
 
-binList = [0,1,2,3,4,5];
-
+binList = [1,2,3,4,5];
+binCount =13
+maxSize = 5
 #fill binList with all possible values-orders
 
 solutions = []
-targetFraction = 0.5
+alpha = 0.5
 #
-def search(index, visited: list, currentBallCount):
-    #deal with current index value
-    if(index in visited):
+def search_recurse(i, v: list, bC):
+    #deal with current i value
+    if(i in v):
         return
     else:
-        currentBallCount += binList[index]
-        visited.append(index)
-    if (len(visited) > len(binList)*targetFraction): #if we've filled half the bins
-        solutions.append(currentBallCount)
+        bC += binList[i]
+        v.append(i)
+    if (len(v) > len(binList)*alpha): #if we've filled half the bins
+#         print("v: " + str(len(v)) + "|" + str(len(binList))+ "|" + str(alpha))
+        solutions.append(bC)
         return
     
     for i in range (1, len(binList)-1):
-        search((index-i)%len(binList), visited[:], currentBallCount) #utilize the fact that list[-1] gets last element
-    
+        search_recurse((i-i)%len(binList), v[:], bC) #utilize the fact that list[-1] gets last element
 
-# myList = [0,1,2,3,4,5] #i=2, 2-6=-4
-# print(myList[-6]) ##prints 2
+def search():
+    for i in range(0,len(binList)):
+        search_recurse(i, [], 0)
 
-for i in range(0,len(binList)):
-    search(i, [], 0)
+def createBinList(bins, maxSize):
+    i = 0
+    binList.clear()
+    while(i < bins):
+        binList.append(random.randint(0, maxSize))
+        i = i + 1
     
+createBinList(binCount, maxSize)
+search()
+
 print("Solutions: " + str(solutions))
 print("Unique solutions:" + str(set(solutions)))
-print("min: " + str(min(solutions)))
